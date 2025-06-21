057b4ceea31141a0d0378fd31031de91aba3481e
princoo <princeineza@gmail.com>
2025-06-22 01:01:31 +0200

    refactor(headphones/page): update parameter handling for async function

--- src/app/headphones/[slug]/page.tsx
+++ src/app/headphones/[slug]/page.tsx
@@ -11,8 +11,13 @@ export async function generateStaticParams() {
     slug: product.slug,
   }));
 }
-export default function page({ params }: { params: { slug: string } }) {
-  const product = getProductBySlug(params.slug);
+export default async function page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const product = getProductBySlug(slug);
   if (!product) {
     return <div>Product not found</div>;
   }
